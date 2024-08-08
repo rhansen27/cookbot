7d4ff3cab47af0e236a709c65c6d814e2581fd6a
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-08-07 20:34:45 -0500

    ant design and buttons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import LikeButton from "../components/Likebutton";
+import DislikeButton from "../components/DislikeButton";
+
 const Home = () => {
   return (
     <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 my-3">
           <h1>Home</h1>
+          <LikeButton />
+          <DislikeButton />
         </div>
       </div>
     </main>
