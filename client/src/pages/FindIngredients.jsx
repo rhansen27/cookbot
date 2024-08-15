d62d49713ae34aed503edc554e897740a48605c0
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-08-14 20:53:14 -0500

    page name changed to AI name

--- client/src/pages/FindIngredients.jsx
+++ client/src/pages/FindIngredients.jsx
@@ -6,7 +6,7 @@ const FindIngredient = () => {
     <main>
       <div className="flex-row justify-center">
         <div className="col-12 col-md-10 my-3">
-          <h2>Find Recipe with Ingredients</h2>
+          <h2>AI Recipe with Ingredients</h2>
           <IngredientBot />
         </div>
       </div>
