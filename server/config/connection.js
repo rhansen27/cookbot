68034e7ad575e849ca942cd3862455c75dc520fb
Ryan Hansen <ryanhansen@Ryans-MacBook-Pro.local>
2024-08-19 16:52:54 -0500

    deploying to render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 console.log(process.env.MONGODB_URI);
-
+// for merge
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/cookbot"
 );
